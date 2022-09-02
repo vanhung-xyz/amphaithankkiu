a5ae6a48fe4a75d69183295a5609fa87f00a9162
vanhung-xyz <112147951+vanhung-xyz@users.noreply.github.com>
2022-09-02 23:37:50 +0700

    Add files via upload

---
+++ bai8vd1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int main(int argc, char *argv[]) 
+{
+	 float com = 0 , sales_amt;
+	 printf("Enter The Sales Amount: ");
+	 scanf("%f",&sales_amt);
+	 if("sales_amt >= 10000")
+	 com = sales_amt * 0.1;
+	 printf("\n Commission = %f",com);
+	 
+}
+return 0;
+}
+
---
+++ bai8vd2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int main(int argc, char *argv[])
+ {  
+  int Num1,Num2;
+  Num1 = 540;
+  Num2 = 243;
+  if(Num1 > Num2)
+  printf("\n The Greater Number Is: %d",Num1);
+  else
+  printf("\n The Greater Number Is: %d",Num2);
+	return 0;
+}
+
+return 0;
+}
+
